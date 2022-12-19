b434bbdaae990c5b3ff3d75d717a0c135fee9aaa
Abraham <abrahamcchua@gmail.com>
2022-12-19 16:48:37 +0800

    (Update) Added preprocessing to the unlabelled NER dataset

--- WebsiteLinkParser/WebsiteLinkParser/spiders/recipes.py
+++ WebsiteLinkParser/WebsiteLinkParser/spiders/recipes.py
@@ -1,18 +1,31 @@
-import scrapy
+import string
 import json
+
+import scrapy
 
 class RecipesSpider(scrapy.Spider):
     name = 'recipes'
     with open('links.json', 'r') as f:
         data = json.load(f)
     start_urls = [data_iter['link'] for data_iter in data]
-
+    
+    
+    def preprocess(self, instruction):
+        # Combines lists and removes punctuations
+        instruction = ' '.join(instruction)
+        return instruction.translate(str.maketrans('', '', string.punctuation))
+     
+    
+    def remove_space(self, list):
+        # Removes leading and trailing space
+        return [sentence.strip() for sentence in list]
+    
+    
     def parse(self, response):
         # For cases where the css format is not the same 
-        secondary_list = response.css("li.wprm-recipe-instruction").css("div.wprm-recipe-instruction-text").css('span::text').getall()
-        instruction = response.css('div.wprm-recipe-instruction-text::text').getall() + secondary_list
-        #
-        instruction = ' '.join(instruction)
+        alternative_format_list = response.css("li.wprm-recipe-instruction").css("div.wprm-recipe-instruction-text").css('span::text').getall()
+        instruction = response.css('div.wprm-recipe-instruction-text::text').getall() + alternative_format_list
+        instruction = self.preprocess(self.remove_space(instruction))
         # Don't parse the website if there are no instructions
         if len(instruction) != 0:
             yield {
